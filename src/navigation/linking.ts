7a95e3180f5c728133fc7e2753a0cfcd7c6a0591
Vytautas Saulis <vaulis11@gmail.com>
2024-12-22 19:10:50 +0200

    Deep linking

--- src/navigation/linking.ts
+++ src/navigation/linking.ts
@@ -1,15 +1,16 @@
+import { API_URL } from '@env';
 import { RootParamsList } from '@navigation/navigation';
 import { LinkingOptions } from '@react-navigation/native';
 
 import { mainRoute, openBankingRoute } from './types';
 
 const linking: LinkingOptions<RootParamsList> = {
-  prefixes: ['budgetapp://'],
+  prefixes: [`${API_URL}/app/`],
   config: {
     screens: {
       [mainRoute]: {
         screens: {
-          [openBankingRoute]: openBankingRoute,
+          [openBankingRoute]: 'open-banking',
         },
       },
     },
