e99ce0425acd2c60374e9f8011dede92bc447111
Vytautas Saulis <vaulis11@gmail.com>
2024-12-26 19:09:56 +0200

    Lint fix

--- src/features/invitations/components/InvitationDetails.tsx
+++ src/features/invitations/components/InvitationDetails.tsx
@@ -1,5 +1,4 @@
 import { Invitation } from '@api/clients/invitations/types';
-import { useInvitationsTranslations } from '@i18n/hooks';
 import { padding } from '@styles/lightTheme';
 import React from 'react';
 import { RefreshControl, ScrollView, StyleProp, Text, ViewStyle } from 'react-native';
@@ -18,19 +17,15 @@ const InvitationDetails = ({
   onRefresh,
   isRefreshing,
   style,
-}: InvitationDetailsProps) => {
-  const { t } = useInvitationsTranslations();
-
-  return (
-    <ScrollView
-      style={style}
-      refreshControl={
-        <RefreshControl onRefresh={onRefresh} refreshing={isLoading || isRefreshing} />
-      }
-      contentContainerStyle={padding('full')('m')}>
-      <Text>{JSON.stringify(invitation, null, 2)}</Text>
-    </ScrollView>
-  );
-};
+}: InvitationDetailsProps) => (
+  <ScrollView
+    style={style}
+    refreshControl={
+      <RefreshControl onRefresh={onRefresh} refreshing={isLoading || isRefreshing} />
+    }
+    contentContainerStyle={padding('full')('m')}>
+    <Text>{JSON.stringify(invitation, null, 2)}</Text>
+  </ScrollView>
+);
 
 export default InvitationDetails;
